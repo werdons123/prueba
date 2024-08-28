eebdffcd2e7a36a1a61d3817e39b764a59f9cfdd
werdons123 <94635164+werdons123@users.noreply.github.com>
2024-08-28 12:29:22 -0500

    Add files via upload

---
+++ proyectoGrupo1/src/main/java/pe/edu/upc/proyectogrupo1/ProyectoGrupo1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.proyectogrupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGrupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGrupo1Application.class, args);
+	}
+
+}
---
+++ proyectoGrupo1/src/test/java/pe/edu/upc/proyectogrupo1/ProyectoGrupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.proyectogrupo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
